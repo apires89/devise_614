aa00c001a2f9a4823643d7b48c984619a6d030c1
andre <andrecabralpires@gmail.com>
2021-05-31 10:12:22 +0100

    devise lecture 1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  #forcing login or sign up on every route!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    ### sign up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    # change information
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
