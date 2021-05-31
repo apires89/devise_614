48e21a3a1dad2ec10a56917a951d1eb62ef27c1c
andre <andrecabralpires@gmail.com>
2021-05-31 10:33:17 +0100

    final changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
   devise_for :users
   root to: 'pages#home'
+  resources :restaurants, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
